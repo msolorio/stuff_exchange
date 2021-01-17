e7af034290e683abcab3d8844effad501c37a3a4
msolorio <msolorio.github@gmail.com>
2021-01-16 21:19:01 -0800

    Adds mongo db connection and creates User model

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const connectionString = 'mongodb://localhost:27017/stuff-exchange';
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(connectionString, mongooseOptions);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${connectionString}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`Mongoose disconnected to ${connectionString}`);
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Error on mongoose connection:', err);
+});
+
+module.exports = {
+  User
+};
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
 const PORT = 4000;
+const authController = require('./controllers/authController');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'milo the barking dog',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+app.use('/', authController);
 
 app.get('/', (req, res) => {
   res.send('Home page');
